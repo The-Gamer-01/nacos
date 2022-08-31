378724112e7d7292775d05b23c2071b7972b2a2d
绿名蒟蒻 <843497509@qq.com>
2022-08-31 18:51:45 +0800

    Update MapperManager, Mapper and MapperManagerTest.

--- plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/manager/MapperManager.java
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/manager/MapperManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.nacos.plugin.datasource.manager;
 
 import com.alibaba.nacos.common.spi.NacosServiceLoader;
@@ -34,15 +50,19 @@ public class MapperManager implements InitAndClosable {
     @Override
     public void loadInitial() {
         Collection<Mapper> mappers = NacosServiceLoader.load(Mapper.class);
-        for(Mapper mapper : mappers) {
+        for (Mapper mapper : mappers) {
             MAPPER_SPI_MAP.put(mapper.getTableName(), mapper);
             LOGGER.info("[MapperManager] Load Mapper({}) tableName({}) successfully.",
                     mapper.getClass(), mapper.getTableName());
         }
     }
     
+    /**
+     * To join mapper in MAPPER_SPI_MAP.
+     * @param mapper The mapper you want to join.
+     */
     public static synchronized void join(Mapper mapper) {
-        if(Objects.isNull(mapper)) {
+        if (Objects.isNull(mapper)) {
             return;
         }
         MAPPER_SPI_MAP.put(mapper.getTableName(), mapper);
---
+++ plugin/datasource/src/test/java/com/alibaba/nacos/plugin/datasource/manager/MapperManagerTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.plugin.datasource.manager;
+
+import com.alibaba.nacos.plugin.datasource.mapper.base.Mapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
+public class MapperManagerTest {
+    
+    @Test
+    public void testInstance() {
+        MapperManager mapperManager = MapperManager.instance();
+        Assert.assertNotNull(mapperManager);
+    }
+    
+    @Test
+    public void testJoin() {
+        MapperManager.join(() -> "test");
+        Assert.assertNotNull(MapperManager.instance().findMapper("test"));
+    }
+    
+    @Test
+    public void testFindMapper() {
+        MapperManager mapperManager = MapperManager.instance();
+        Optional<Mapper> test = mapperManager.findMapper("test");
+        Assert.assertFalse(test.isPresent());
+    }
+}
